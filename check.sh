f2248674d5c677a56febeaa52b74eb98d2a19550
wren gayle romano <wren@community.haskell.org>
2016-08-11 15:08:00 -0700

    Added script for checking staleness of this repo

---
+++ check.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# A quick script for checking whether this repo is in sync with my
+# actual homedir.
+#
+# wren gayle romano <wren@cpan.org>                 ~ 2016.08.11
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# (I don't use the git repo directly on my main machine, to avoid
+# issues about things like irssi's cleartext passwords ending up
+# in the repo. Ideally, I'd have an "install" script to manage this
+# pattern everywhere. Perhaps we could use something like this to
+# help: <https://github.com/oohlaf/dotsecrets>)
+
+
+# N.B., must run from the git root directory!
+# TODO: fix that.
+# BUG: still follows links and stuff, giving "Common subdirectories" outputs
+find . ! -path './.git*' ! -type d \
+    | while read line ; do diff -q {.,~}/"$line" ; done \
+    | hilight differ
+
+exit 0
