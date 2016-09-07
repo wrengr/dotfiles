87bc78cc1ee72562cb2e8956d1e4e2e635e71782
wren gayle romano <wren@community.haskell.org>
2016-09-06 20:59:21 -0700

    _check_dotfiles_repo.sh: added some more todo comments

--- local/bin/_check_dotfiles_repo.sh
+++ local/bin/_check_dotfiles_repo.sh
@@ -2,7 +2,7 @@
 # A quick script for checking whether this repo is in sync with my
 # actual homedir.
 #
-# wren gayle romano <wren@cpan.org>                 ~ 2016.08.11
+# wren gayle romano <wren@cpan.org>                 ~ 2016.09.06
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # (I don't use the git repo directly on my main machine, to avoid
 # issues about things like irssi's cleartext passwords ending up
@@ -13,6 +13,7 @@
 
 # N.B., must run from the git root directory!
 # TODO: fix that.
+# TODO: at the very least, warn if run from ~ and ~/.git doesn't exist
 # BUG: still follows links and stuff, giving "Common subdirectories" outputs
 find . ! -path './.git*' ! -type d \
     | while read line ; do diff -q {.,~}/"$line" ; done \
