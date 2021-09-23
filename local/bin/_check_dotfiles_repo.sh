58f3cdc450378316f05cd75e07ce01b7807da3fd
wren romano <2998727+wrengr@users.noreply.github.com>
2021-09-22 23:23:27 -0700

    _check_dotfiles_repo.sh: cleaning up the output a bit

--- local/bin/_check_dotfiles_repo.sh
+++ local/bin/_check_dotfiles_repo.sh
@@ -2,7 +2,7 @@
 # A quick script for checking whether this repo is in sync with my
 # actual homedir.
 #
-# wren gayle romano <wren@cpan.org>                 ~ 2016.06.15
+# wren gayle romano <wren@cpan.org>                 ~ 2021.09.22
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # (I don't use the git repo directly on my main machine, to avoid
 # issues about things like irssi's cleartext passwords ending up
@@ -11,7 +11,7 @@
 # help: <https://github.com/oohlaf/dotsecrets>)
 
 
-# N.B., must run from the git root directory!
+# BUG: must run from the git root directory!
 # TODO: fix that.
 # TODO: at the very least, warn if run from ~ and ~/.git doesn't exist
 # BUG: still follows links and stuff, giving "Common subdirectories" outputs
@@ -26,6 +26,6 @@ find . ! -path './.git*' ! -type d \
 			echo "# No such file: ~/$line" ; \
 		fi ; \
 	done \
-	| hilight differ
+	| perl -ple 's@^Files ./(.*) and '"$HOME"'/\1 differ$@Differ $1@'
 
 exit 0
