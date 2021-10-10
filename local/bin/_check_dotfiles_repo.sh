59f5df001d7597846c73382884fae46b650ab19b
wren romano <2998727+wrengr@users.noreply.github.com>
2021-10-09 20:28:16 -0700

    _check_dotfiles_repo.sh: Added $gitdir hack, and ~/.vim/ missing check

--- local/bin/_check_dotfiles_repo.sh
+++ local/bin/_check_dotfiles_repo.sh
@@ -2,7 +2,7 @@
 # A quick script for checking whether this repo is in sync with my
 # actual homedir.
 #
-# wren gayle romano <wren@cpan.org>                 ~ 2021.09.22
+# wren gayle romano <wren@cpan.org>                 ~ 2021.10.09
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # (I don't use the git repo directly on my main machine, to avoid
 # issues about things like irssi's cleartext passwords ending up
@@ -10,22 +10,33 @@
 # pattern everywhere. Perhaps we could use something like this to
 # help: <https://github.com/oohlaf/dotsecrets>)
 
+# HACK: since we must run this from the git rootdir, and since I
+# only ever run this on mayari, we'll just hard-code switching to
+# the known git-rootdir there.
+gitdir="$HOME/code/dotfiles"
+cd $gitdir
 
-# BUG: must run from the git root directory!
-# TODO: fix that.
-# TODO: at the very least, warn if run from ~ and ~/.git doesn't exist
 # BUG: still follows links and stuff, giving "Common subdirectories" outputs
 # HACK: we just explicitly filter those out for now.
-find . ! -path './.git*' ! -type d \
+find . ! -path './.git/*' ! -type d \
 	| sed 's@^\./@@' \
 	| while read line ; do \
 		if [ -e ~/"$line" ]; then \
 			diff -q {.,~}/"$line" \
-			| grep -v '^Common subdirectories:' ; \
+			| grep -v '^Common subdirectories:' \
+	        | perl -ple 's@^Files ./(.*) and '"$HOME"'/\1 differ$@Differ    $1@' ; \
 		else \
-			echo "# No such file: ~/$line" ; \
+			echo "Unknown ~/$line" ; \
 		fi ; \
-	done \
-	| perl -ple 's@^Files ./(.*) and '"$HOME"'/\1 differ$@Differ $1@'
+	done
+
+echo
+cd ~
+find .vim ! -path '.vim/bundle/*' ! -type d \
+	| while read line ; do \
+		if [ -e "$gitdir/$line" ]; then : ; else \
+			echo "Missing ~/$line" ; \
+		fi ; \
+	done
 
 exit 0
